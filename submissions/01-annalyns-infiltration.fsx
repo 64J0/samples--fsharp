0b644cfe4ab76c94bf89a57e7a2394f465a0e834
64J0 <vinigaio97@gmail.com>
2023-02-28 19:54:06 -0300

    feat: bootstrap repo

---
+++ submissions/01-annalyns-infiltration.fsx
@@ -0,0 +1,17 @@
+module AnnalynsInfiltration
+
+let canFastAttack (knightIsAwake: bool) =
+    not knightIsAwake
+
+let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) =
+    knightIsAwake ||
+    prisonerIsAwake ||
+    archerIsAwake
+
+let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool) =
+    (not archerIsAwake) &&
+    prisonerIsAwake
+
+let canFreePrisoner (knightIsAwake: bool)  (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool) =
+    ((not (knightIsAwake || archerIsAwake)) && prisonerIsAwake) ||
+    ((not archerIsAwake) && petDogIsPresent)
