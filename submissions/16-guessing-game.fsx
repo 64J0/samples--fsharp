f4b5ddca63cf4589b9f76dadae998f18e85e620c
64J0 <vinigaio97@gmail.com>
2023-03-24 23:35:36 -0300

    chore: add 16th submission - guessing game

---
+++ submissions/16-guessing-game.fsx
@@ -0,0 +1,12 @@
+module GuessingGame
+
+let reply (guess: int): string =
+    match guess with
+    | 42 -> "Correct"
+    | 41
+    | 43 -> "So close"
+    | x when (x < 41) -> "Too low"
+    | x when (x > 43) -> "Too high"
+    | _ -> "Invalid"
+
+reply 41
