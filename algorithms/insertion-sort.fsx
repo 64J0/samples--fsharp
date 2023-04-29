333ca6e0cc7e5c59f42cb0795bb2f9066928e93e
64J0 <vinigaio97@gmail.com>
2023-04-29 10:37:45 -0300

    feat: add recursive insertion sort algorithm in fsharp

---
+++ algorithms/insertion-sort.fsx
@@ -0,0 +1,29 @@
+module InsertionSort
+
+// Recursive implementation of the insertion sort algorithm in F#
+
+let rec insertionSort (arrIn: int array) (arrOut: int array) =
+      match arrIn with
+      | [||] -> arrOut
+      | _ ->
+          let el = Array.head arrIn
+          let newArrIn = Array.tail arrIn
+          let indexBiggerNum =
+              arrOut
+              |> Array.tryFindIndex (fun x -> x >= el)
+          let newArrOut =
+              match indexBiggerNum with
+              | Some i ->
+                  Array.concat [ arrOut.[0 .. (i-1)]; [| el |]; arrOut.[i ..] ]
+              | None ->
+                  Array.concat [arrOut; [| el |]]
+
+          insertionSort (newArrIn) (newArrOut)
+
+let arrIn = [| 5; 2; 4; 6; 1; 3 |]
+let arrOut = [||]
+insertionSort arrIn arrOut
+
+
+let arrIn' = [| 5; 2; 4; 6; 1; 3; 3; 3; 10 |]
+insertionSort arrIn' arrOut
