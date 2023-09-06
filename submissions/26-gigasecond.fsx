eed3ce1233a95ef6baaf0ea975753735bc5aa03f
64J0 <vinigaio97@gmail.com>
2023-09-05 21:34:56 -0300

    lint: fantomas

--- submissions/26-gigasecond.fsx
+++ submissions/26-gigasecond.fsx
@@ -5,8 +5,7 @@ open System
 [<Literal>]
 let GIGA_SECOND = 1_000_000_000
 
-let add (beginDate: DateTime) : DateTime =
-    GIGA_SECOND |> beginDate.AddSeconds
+let add (beginDate: DateTime) : DateTime = GIGA_SECOND |> beginDate.AddSeconds
 
 let result = add (DateTime(2011, 4, 25))
 printfn "Result: %A" result
--- submissions/32-strain.fsx
+++ submissions/32-strain.fsx
@@ -5,23 +5,19 @@ let initialState: list<'T> = List.empty
 let keep (pred: 'T -> bool) (xs: seq<'T>) =
     xs
     |> Seq.toList
-    |> List.fold 
-        (fun (state: list<'T>) (x: 'T) -> 
-            if (pred x) then ([x] |> List.append state)
-            else state) 
+    |> List.fold
+        (fun (state: list<'T>) (x: 'T) -> if (pred x) then ([ x ] |> List.append state) else state)
         initialState
 
 let discard (pred: 'T -> bool) (xs: seq<'T>) =
     xs
     |> Seq.toList
-    |> List.fold 
-        (fun (state: list<'T>) (x: 'T) -> 
-            if not (pred x) then ([x] |> List.append state)
-            else state) 
+    |> List.fold
+        (fun (state: list<'T>) (x: 'T) -> if not (pred x) then ([ x ] |> List.append state) else state)
         initialState
 
 keep (fun x -> x < 10) []
 
-set [1; 2; 3] |> keep (fun x -> x < 10) |> Seq.toList
+set [ 1; 2; 3 ] |> keep (fun x -> x < 10) |> Seq.toList
 
-[|1; 2; 3|] |> keep (fun x -> x % 2 <> 0) |> Seq.toList
+[| 1; 2; 3 |] |> keep (fun x -> x % 2 <> 0) |> Seq.toList
