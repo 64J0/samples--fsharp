e3d6b9a3c9030a2e862029ad00f5cdcc7b6e9b72
64J0 <vinigaio97@gmail.com>
2024-12-04 21:55:13 -0300

    fantomas lint

--- submissions/queen-attack.fsx
+++ submissions/queen-attack.fsx
@@ -1,16 +1,13 @@
-let create (position: int * int): bool =
+let create (position: int * int) : bool =
     let x, y = position
-    x >= 0
-    && x < 8
-    && y >= 0
-    && y < 8
-    
+    x >= 0 && x < 8 && y >= 0 && y < 8
+
 let canAttack (queen1: int * int) (queen2: int * int) : bool =
     let checkHorizontal (queen1, queen2) : bool =
         let _, y1 = queen1
         let _, y2 = queen2
         y1 = y2
-    
+
     let checkVertical (queen1, queen2) : bool =
         let x1, _ = queen1
         let x2, _ = queen2
@@ -21,7 +18,7 @@ let canAttack (queen1: int * int) (queen2: int * int) : bool =
     let checkDiagonal (queen1, queen2) : bool =
         let x1, y1 = queen1
         let x2, y2 = queen2
-        abs(x1 - x2) = abs(y1 - y2)
+        abs (x1 - x2) = abs (y1 - y2)
 
     checkHorizontal (queen1, queen2)
     || checkVertical (queen1, queen2)
